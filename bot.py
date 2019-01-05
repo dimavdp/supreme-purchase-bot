bc9d66c148190af78f016f15944a0971ad828f28
dimavdp <dimavdp@gmail.com>
2019-01-06 00:56:08 +0300

    First commit

---
+++ bot.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+from config import keys
+import time
+
+# It's a Python time wrapper, actually used to display time of function execution from start to finish
+def timeme(method):
+    def wrapper(*args, **kw):
+        startTime = int(round(time.time() * 1000))
+        result = method(*args, **kw)
+        endTime = int(round(time.time() * 1000))
+        print((endTime - startTime)/1000, 's')
+        return result
+    return wrapper
+
+# will cookies improve load time?
+#options = webdriver.ChromeOptions()
+#options.add_argument('user-data-dir=www.supremenewyork.com')
+
+@timeme
+def order():
+    # add to cart
+    driver.find_element_by_name('commit').click()
+
+    # wait for checkout button element to load
+    time.sleep(.5)
+    checkout_element = driver.find_element_by_class_name('checkout')
+    checkout_element.click()
+
+    #   fill out checkout screen fields
+    #   using Inspect button on fields in Chrome
+    driver.find_element_by_xpath('//*[@id="order_billing_name"]').send_keys(keys['name'])
+    driver.find_element_by_xpath('//*[@id="order_email"]').send_keys(keys['email'])
+    driver.find_element_by_xpath('//*[@id="order_tel"]').send_keys(keys['phone_number'])
+    driver.find_element_by_xpath('//*[@id="bo"]').send_keys(keys['street_address'])
+    driver.find_element_by_xpath('//*[@id="order_billing_city"]').send_keys(keys['city'])
+    driver.find_element_by_xpath('//*[@id="order_billing_zip"]').send_keys(keys['zip_code'])
+    driver.find_element_by_xpath('//*[@id="cnb"]').send_keys(keys['card_number'])
+    driver.find_element_by_xpath('//*[@id="vval"]').send_keys(keys['card_cvv'])
+    process_payment = driver.find_element_by_xpath('//*[@id="pay"]/input')
+    process_payment.click()
+
+if __name__ == '__main__':
+        # load chrome
+    driver = webdriver.Chrome('./chromedriver')
+
+    # get product url
+    driver.get(keys['product_url'])
+    order()
---
+++ config.py
@@ -0,0 +1,11 @@
+keys = {
+        "product_url": "https://www.supremenewyork.com/shop/sweatshirts/yw4mifo2y",
+        "name": "Ky Nguen",
+        "email": "test@gmail.com",
+        "phone_number": "6789998212",
+        "street_address": "Arbat St.",
+        "city": "Moscow", 
+        "zip_code": "91601",
+        "card_cvv": "666",
+        "card_number": "1524615272839913"
+}
